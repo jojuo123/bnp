e448e6787cf0b8b75ac4ae77c768ff3b2586f043
Duy Vu-Tran <vtduy3100@gmail.com>
2021-10-29 11:32:42 +0700

    Add files via upload

---
+++ src/benepar/encoder.py
@@ -0,0 +1,316 @@
+import numpy as np
+import sys
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.init as init
+import transformers
+
+class FeatureDropoutFunction(torch.autograd.function.InplaceFunction):
+    @classmethod
+    def forward(cls, ctx, input, batch_idxs, p=0.5, train=False, inplace=False):
+        if p < 0 or p > 1:
+            raise ValueError("dropout probability has to be between 0 and 1, "
+                             "but got {}".format(p))
+
+        ctx.p = p
+        ctx.train = train
+        ctx.inplace = inplace
+
+        if ctx.inplace:
+            ctx.mark_dirty(input)
+            output = input
+        else:
+            output = input.clone()
+
+        if ctx.p > 0 and ctx.train:
+            ctx.noise = input.new().resize_(batch_idxs.batch_size, input.size(1))
+            if ctx.p == 1:
+                ctx.noise.fill_(0)
+            else:
+                ctx.noise.bernoulli_(1 - ctx.p).div_(1 - ctx.p)
+            ctx.noise = ctx.noise[batch_idxs.batch_idxs_torch, :]
+            output.mul_(ctx.noise)
+
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.p > 0 and ctx.train:
+            return grad_output.mul(ctx.noise), None, None, None, None
+        else:
+            return grad_output, None, None, None, None
+
+#
+class FeatureDropout(nn.Module):
+    """
+    Feature-level dropout: takes an input of size len x num_features and drops
+    each feature with probabibility p. A feature is dropped across the full
+    portion of the input that corresponds to a single batch element.
+    """
+    def __init__(self, p=0.5, inplace=False):
+        super().__init__()
+        if p < 0 or p > 1:
+            raise ValueError("dropout probability has to be between 0 and 1, "
+                             "but got {}".format(p))
+        self.p = p
+        self.inplace = inplace
+
+    def forward(self, input, batch_idxs):
+        return FeatureDropoutFunction.apply(input, batch_idxs, self.p, self.training, self.inplace)
+
+#
+class LayerNormalization(nn.Module):
+    def __init__(self, d_hid, eps=1e-3, affine=True):
+        super(LayerNormalization, self).__init__()
+
+        self.eps = eps
+        self.affine = affine
+        if self.affine:
+            self.a_2 = nn.Parameter(torch.ones(d_hid), requires_grad=True)
+            self.b_2 = nn.Parameter(torch.zeros(d_hid), requires_grad=True)
+
+    def forward(self, z):
+        if z.size(-1) == 1:
+            return z
+
+        mu = torch.mean(z, keepdim=True, dim=-1)
+        sigma = torch.std(z, keepdim=True, dim=-1)
+        ln_out = (z - mu.expand_as(z)) / (sigma.expand_as(z) + self.eps)
+        if self.affine:
+            ln_out = ln_out * self.a_2.expand_as(ln_out) + self.b_2.expand_as(ln_out)
+
+        return ln_out
+
+#
+class ScaledAttention(nn.Module):
+    def __init__(self, hparams, attention_dropout=0.1):
+        super(ScaledAttention, self).__init__()
+        self.hparams = hparams
+        self.temper = hparams.d_model ** 0.5
+        self.dropout = nn.Dropout(attention_dropout)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, q, k, v, attn_mask=None):
+        # q: [batch, slot, feat]
+        # k: [batch, slot, feat]
+        # v: [batch, slot, feat]
+
+        attn = torch.bmm(q, k.transpose(1, 2)) / self.temper
+
+
+        if attn_mask is not None:
+            assert attn_mask.size() == attn.size(), \
+                    'Attention mask shape {} mismatch ' \
+                    'with Attention logit tensor shape ' \
+                    '{}.'.format(attn_mask.size(), attn.size())
+
+            attn.data.masked_fill_(attn_mask, -float('inf'))
+
+        attn = self.softmax(attn.transpose(1, 2)).transpose(1, 2)
+        attn = self.dropout(attn)
+        output = torch.bmm(attn, v)
+
+        return output, attn
+
+# %%
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_model, attention_dropout=0.1):
+        super(ScaledDotProductAttention, self).__init__()
+        self.temper = d_model ** 0.5
+        self.dropout = nn.Dropout(attention_dropout)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, v, attn_mask=None):
+        # q: [batch, slot, feat] or (batch * d_l) x max_len x d_k
+        # k: [batch, slot, feat] or (batch * d_l) x max_len x d_k
+        # v: [batch, slot, feat] or (batch * d_l) x max_len x d_v
+        # q in LAL is (batch * d_l) x 1 x d_k
+
+        attn = torch.bmm(q, k.transpose(1, 2)) / self.temper # (batch * d_l) x max_len x max_len
+        # in LAL, gives: (batch * d_l) x 1 x max_len
+        # attention weights from each word to each word, for each label
+        # in best model (repeated q): attention weights from label (as vector weights) to each word
+
+        if attn_mask is not None:
+            assert attn_mask.size() == attn.size(), \
+                    'Attention mask shape {} mismatch ' \
+                    'with Attention logit tensor shape ' \
+                    '{}.'.format(attn_mask.size(), attn.size())
+
+            attn.data.masked_fill_(attn_mask, -float('inf'))
+
+        attn = self.softmax(attn)
+        # Note that this makes the distribution not sum to 1. At some point it
+        # may be worth researching whether this is the right way to apply
+        # dropout to the attention.
+        # Note that the t2t code also applies dropout in this manner
+        attn = self.dropout(attn)
+        output = torch.bmm(attn, v) # (batch * d_l) x max_len x d_v
+        # in LAL, gives: (batch * d_l) x 1 x d_v
+
+        return output, attn
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi-head attention module
+    """
+
+    def __init__(self, hparams, n_head, d_model, d_k, d_v, residual_dropout=0.1, attention_dropout=0.1, d_positional=None):
+        super(MultiHeadAttention, self).__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+        self.hparams = hparams
+
+        if d_positional is None:
+            self.partitioned = False
+        else:
+            self.partitioned = True
+
+        if self.partitioned:
+            self.d_content = d_model - d_positional
+            self.d_positional = d_positional
+
+            self.w_qs1 = nn.Parameter(torch_t.FloatTensor(n_head, self.d_content, d_k // 2))
+            self.w_ks1 = nn.Parameter(torch_t.FloatTensor(n_head, self.d_content, d_k // 2))
+            self.w_vs1 = nn.Parameter(torch_t.FloatTensor(n_head, self.d_content, d_v // 2))
+
+            self.w_qs2 = nn.Parameter(torch_t.FloatTensor(n_head, self.d_positional, d_k // 2))
+            self.w_ks2 = nn.Parameter(torch_t.FloatTensor(n_head, self.d_positional, d_k // 2))
+            self.w_vs2 = nn.Parameter(torch_t.FloatTensor(n_head, self.d_positional, d_v // 2))
+
+            init.xavier_normal_(self.w_qs1)
+            init.xavier_normal_(self.w_ks1)
+            init.xavier_normal_(self.w_vs1)
+
+            init.xavier_normal_(self.w_qs2)
+            init.xavier_normal_(self.w_ks2)
+            init.xavier_normal_(self.w_vs2)
+        else:
+            self.w_qs = nn.Parameter(torch_t.FloatTensor(n_head, d_model, d_k))
+            self.w_ks = nn.Parameter(torch_t.FloatTensor(n_head, d_model, d_k))
+            self.w_vs = nn.Parameter(torch_t.FloatTensor(n_head, d_model, d_v))
+
+            init.xavier_normal_(self.w_qs)
+            init.xavier_normal_(self.w_ks)
+            init.xavier_normal_(self.w_vs)
+
+        self.attention = ScaledDotProductAttention(d_model, attention_dropout=attention_dropout)
+        self.layer_norm = LayerNormalization(d_model)
+
+        if not self.partitioned:
+            # The lack of a bias term here is consistent with the t2t code, though
+            # in my experiments I have never observed this making a difference.
+            self.proj = nn.Linear(n_head*d_v, d_model, bias=False)
+        else:
+            self.proj1 = nn.Linear(n_head*(d_v//2), self.d_content, bias=False)
+            self.proj2 = nn.Linear(n_head*(d_v//2), self.d_positional, bias=False)
+
+        self.residual_dropout = FeatureDropout(residual_dropout)
+
+    def split_qkv_packed(self, inp, qk_inp=None):
+        v_inp_repeated = inp.repeat(self.n_head, 1).view(self.n_head, -1, inp.size(-1)) # n_head x len_inp x d_model
+        if qk_inp is None:
+            qk_inp_repeated = v_inp_repeated
+        else:
+            qk_inp_repeated = qk_inp.repeat(self.n_head, 1).view(self.n_head, -1, qk_inp.size(-1))
+
+        if not self.partitioned:
+            q_s = torch.bmm(qk_inp_repeated, self.w_qs) # n_head x len_inp x d_k
+            k_s = torch.bmm(qk_inp_repeated, self.w_ks) # n_head x len_inp x d_k
+            v_s = torch.bmm(v_inp_repeated, self.w_vs) # n_head x len_inp x d_v
+        else:
+            q_s = torch.cat([
+                torch.bmm(qk_inp_repeated[:,:,:self.d_content], self.w_qs1),
+                torch.bmm(qk_inp_repeated[:,:,self.d_content:], self.w_qs2),
+                ], -1)
+            k_s = torch.cat([
+                torch.bmm(qk_inp_repeated[:,:,:self.d_content], self.w_ks1),
+                torch.bmm(qk_inp_repeated[:,:,self.d_content:], self.w_ks2),
+                ], -1)
+            v_s = torch.cat([
+                torch.bmm(v_inp_repeated[:,:,:self.d_content], self.w_vs1),
+                torch.bmm(v_inp_repeated[:,:,self.d_content:], self.w_vs2),
+                ], -1)
+        return q_s, k_s, v_s
+
+    def pad_and_rearrange(self, q_s, k_s, v_s, batch_idxs):
+        # Input is padded representation: n_head x len_inp x d
+        # Output is packed representation: (n_head * mb_size) x len_padded x d
+        # (along with masks for the attention and output)
+        n_head = self.n_head
+        d_k, d_v = self.d_k, self.d_v
+
+        len_padded = batch_idxs.max_len
+        mb_size = batch_idxs.batch_size
+        q_padded = q_s.new_zeros((n_head, mb_size, len_padded, d_k))
+        k_padded = k_s.new_zeros((n_head, mb_size, len_padded, d_k))
+        v_padded = v_s.new_zeros((n_head, mb_size, len_padded, d_v))
+        invalid_mask = q_s.new_ones((mb_size, len_padded), dtype=DTYPE)
+
+        for i, (start, end) in enumerate(zip(batch_idxs.boundaries_np[:-1], batch_idxs.boundaries_np[1:])):
+            q_padded[:,i,:end-start,:] = q_s[:,start:end,:]
+            k_padded[:,i,:end-start,:] = k_s[:,start:end,:]
+            v_padded[:,i,:end-start,:] = v_s[:,start:end,:]
+            invalid_mask[i, :end-start].fill_(False)
+
+        return(
+            q_padded.view(-1, len_padded, d_k),
+            k_padded.view(-1, len_padded, d_k),
+            v_padded.view(-1, len_padded, d_v),
+            invalid_mask.unsqueeze(1).expand(mb_size, len_padded, len_padded).repeat(n_head, 1, 1),
+            (~invalid_mask).repeat(n_head, 1),
+            )
+
+    def combine_v(self, outputs):
+        # Combine attention information from the different heads
+        n_head = self.n_head
+        outputs = outputs.view(n_head, -1, self.d_v) # n_head x len_inp x d_kv
+
+        if not self.partitioned:
+            # Switch from n_head x len_inp x d_v to len_inp x (n_head * d_v)
+            outputs = torch.transpose(outputs, 0, 1).contiguous().view(-1, n_head * self.d_v)
+
+            # Project back to residual size
+            outputs = self.proj(outputs)
+        else:
+            d_v1 = self.d_v // 2
+            outputs1 = outputs[:,:,:d_v1]
+            outputs2 = outputs[:,:,d_v1:]
+            outputs1 = torch.transpose(outputs1, 0, 1).contiguous().view(-1, n_head * d_v1)
+            outputs2 = torch.transpose(outputs2, 0, 1).contiguous().view(-1, n_head * d_v1)
+            outputs = torch.cat([
+                self.proj1(outputs1),
+                self.proj2(outputs2),
+                ], -1)
+
+        return outputs
+
+    def forward(self, inp, batch_idxs, qk_inp=None):
+        residual = inp
+
+        # While still using a packed representation, project to obtain the
+        # query/key/value for each head
+        q_s, k_s, v_s = self.split_qkv_packed(inp, qk_inp=qk_inp)
+        # n_head x len_inp x d_kv
+
+        # Switch to padded representation, perform attention, then switch back
+        q_padded, k_padded, v_padded, attn_mask, output_mask = self.pad_and_rearrange(q_s, k_s, v_s, batch_idxs)
+        # (n_head * batch) x len_padded x d_kv
+
+        outputs_padded, attns_padded = self.attention(
+            q_padded, k_padded, v_padded,
+            attn_mask=attn_mask,
+            )
+        outputs = outputs_padded[output_mask]
+        # (n_head * len_inp) x d_kv
+        outputs = self.combine_v(outputs)
+        # len_inp x d_model
+
+        outputs = self.residual_dropout(outputs, batch_idxs)
+
+        return self.layer_norm(outputs + residual), attns_padded
